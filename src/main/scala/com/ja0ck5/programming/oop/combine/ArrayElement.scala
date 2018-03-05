da90943ec4582830c17868f0b8dbd4193b4915d1
ja0ck5 <975117619@qq.com>
2018-03-05 18:32:30 +0800

    Scala 组合继承

---
+++ src/main/scala/com/ja0ck5/programming/oop/combine/ArrayElement.scala
@@ -0,0 +1,59 @@
+package com.ja0ck5.programming.oop.combine
+
+/**
+  * 如果省略了 extends 子句,Scala 编译器将隐式地假设你的类扩展自 scala.AnyRef. 这与 Java 的 java.lang.Object 相同
+  * 因此 Element 类隐式得扩展了 AnyRef 类
+  *
+  * 继承(inheritance) 表示超类的所有成员也是子类的成员，但以下两种情况例外:
+  * 1. 超类的私有成员不会被子类继承
+  * 2. 超类中的成员若与子类中实现的成员具有相同名称和参数则不会被子类继承。这种情况被称为子类的成员重写了超类的成员。如果子类的成员是具体的而超类中的是抽象的,
+  * 我们也可以说具体的成员实现(implements) 了抽象成员
+  *
+  * 统一访问原则只是 Scala 在对待字段和方法上比 Java 更统一的一个方面。
+  * 另一个差异是 Scala 里的字段和方法属于相同的命名空间。折让字段可以重写无参数方法。例如:
+  * 可以改变 ArrayElement 类中 contents 的实现将其从一个方法变为一个字段，而无需修改类 Element 中 contents 的抽象方法定义
+  *
+  * 另一方面 Scala 里禁止在同一个类里面用同样的名称定义字段和方法
+  * 一般来说 Scala 仅有两个命名空间: 值(字段,方法,包,单例对象) / 类型
+  * Scala 把字段和方法放进同一个命名空间的理由很明确,因为这样就可以实现使用 val 重写无参数方法
+  *
+  * Scala 包与字段和方法共享相同的命名空间是为了能够引入包，而不仅仅是引入类型名以及单例对象的字段和方法。这样是无法在 Java 中做到的。
+  *
+  *
+  *
+  * Created by Jack on 2018/3/5.
+  */
+class ArrayElement(conts: Array[String]) extends Element {
+  //  override def contents: Array[String] = conts
+  val contents: Array[String] = conts
+}
+
+class Cat {
+  val dangerous = false
+}
+
+class Tiger(override val dangerous: Boolean,
+            private var age: Int) extends Cat {
+
+}
+
+
+class Tiger2(param1: Boolean,
+             param2: Int) extends Cat {
+  override val dangerous = param1
+  private var age = param2
+}
+
+/**
+  * 1. 若所有重写了父类具体成员的子类成员必须带有 override
+  * 2. 若成员实现的是同名的抽象成员时,则 override 是可选的
+  * 3. 若成员并未重写或实现其他基类里的成员则禁用 override 修饰符
+  * @param s
+  */
+class LineElement(s: String) extends ArrayElement(Array(s)) {
+  override def width: Int = s.length
+
+  override def height: Int = 1
+}
+
+
---
+++ src/main/scala/com/ja0ck5/programming/oop/combine/Element.scala
@@ -0,0 +1,31 @@
+package com.ja0ck5.programming.oop.combine
+
+/**
+  * Created by Jack on 2018/3/3.
+  */
+abstract class Element {
+  // 因为元素是二维的字符矩形,所以定义一个成员 contents 指向布局元素的内容
+  def contents: Array[String]
+  // 推荐的惯例是无论何时,只要方法中没有参数并且方法仅能通过读取所包含对象的属性去访问可变状态(特指方法不能改变可变状态),就使用无参数方法
+  // 这个惯例支持统一访问原则(uniform access principle),就是说客户代码不应 由属性是通过字段实现还是方法实现而受影响
+  def height: Int = contents.length
+  // 由此我们可以更改 height 和 width 作为字段来实现
+  // 两组定义从客户的观点来看是完全相同的。唯一的差别是访问字段比调用方法略快。
+//  val height: Int = contents.length
+//  val width: Int = if (height == 0) 0 else contents(0).length
+
+  def width: Int = if (height == 0) 0 else contents(0).length
+
+  // 原则上,Scala 的函数调用中可以省略所有的空括号。
+  // 1 在调用方法超出其调用者对象的属性时,仍推荐写一对空括号。 例如方法执行了 IO 或写入可重新赋值的变量(var) 或读取不是调用者字段的 var
+  // 2 总之无论是直接的还是非直接的使用可变对象,都应该添加空括号。
+  // 3 这种方式是让参数列表扮演一个课件的线索来说明 调用出发了某些有趣的计算，如:
+  "hello".length // 没有副作用,无需()
+  println()// 最好别省略
+
+  // 总之,Scala 鼓励使用将不带参数且没有副作用的方法定义为无参数方法的风格,即省略空括号
+  // 但永远不要定义没有括号的带副作用的方法,因为那样 方法调用看上去会像在使用字段,所以你的客户会很奇怪为什么它带有副作用
+
+
+
+}
